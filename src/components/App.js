aaa41e61fd1abe5cc8b2d7306f28191c83b5e42c
Virajjj2707 <virajvadhane007@gmail.com>
2022-09-11 14:55:06 +0530

    project completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,44 @@
-import React from "react";
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { v4 as uuid } from "uuid";
+import "./App.css";
 import Header from "./header";
-import AddContact from './addcontact';
-import ContactList from './contactlist';
+import AddContact from "./addcontact";
+import ContactList from "./contactlist";
 
 function App() {
+  const LOCAL_STORAGE_KEY = "contacts";
+  const [contacts, setContacts] = useState(
+    JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) ?? []
+  );
 
-const contacts = [
+  const addContactHandler = (contact) => {
+    console.log(contact);
+    setContacts([...contacts, { id: uuid(), ...contact }]);
+  };
 
-  {
-    id: "1",
-    name: "Dispesh",
-    email: "malvia@gmail.com",
-  },
+  const removeContactHandler = (id) => {
+    const newContactList = contacts.filter((contact) => {
+      return contact.id !== id;
+    });
 
-  {
-    id: "2",
-    name: "Nikesh",
-    email: "hashes@gmail.com",
-  },
-];
+    setContacts(newContactList);
+  };
+
+  // useEffect(() => {
+  //   const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+  //   if (retriveContacts) setContacts(retriveContacts);
+  // }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
 
   return (
     <div className="ui container">
       <Header />
-      <AddContact />
-      <ContactList contacts={contacts}/>
+      <AddContact addContactHandler={addContactHandler} />
+      <ContactList contacts={contacts} getContactId={removeContactHandler} />
     </div>
-
   );
 }
 
